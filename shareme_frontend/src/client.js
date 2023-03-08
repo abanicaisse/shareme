3286b0c58b648afd01711307815ccd3db66b9ce4
Aba Nicaisse <abawandjovunicaise@gmail.com>
2023-03-08 11:21:16 +0300

    Implemented the login Logic, though a small error still to be fixed

---
+++ shareme_frontend/src/client.js
@@ -0,0 +1,16 @@
+// Sanity Client side file - to connect the frontend to sanity
+import { createClient } from "@sanity/client";
+import imageUrlBuilder from "@sanity/image-url";
+
+export const client = createClient({
+  projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
+  dataset: "production",
+  // version in tuto: 2021-11-16
+  apiVersion: "2021-11-16",
+  useCdn: true,
+  token: process.env.REACT_APP_SANITY_TOKEN,
+});
+
+const builder = imageUrlBuilder(client);
+
+export const urlFor = (source) => builder.image(source);
